40f2ac327945abd7d51a2d779f3ab120b0f09af6
nickveil <31191433+nickveil@users.noreply.github.com>
2020-08-29 10:18:09 -0500

    added bootstrap and vue

---
+++ resources/js/components/ChatMessage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <p>{{ message }}</p>
+      <small>{{ user }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            message: "This is some message text.",
+            user: "John Doe"
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
